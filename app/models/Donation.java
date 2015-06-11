343dc5daee566f21dbb333a847b5b69eac52b595
Jean Crosse <jeancrosse37@gmail.com>
2015-06-11 14:42:12 +0100

    V0 donation application generated by Play framework

--- app/models/Donation.java
+++ app/models/Donation.java
@@ -1,5 +1,31 @@
 package models;
 
-public class Donation {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Model;
+import play.db.jpa.Blob;
+
+@Entity
+public class Donation extends Model
+{
+	public long received;
+	public String methodDonated;
+	@ManyToOne
+	public User from;
+
+	public Donation(User from, long received, String methodDonated)
+	{
+		this.received = received;
+		this.methodDonated = methodDonated;
+		this.from = from;
+	}
 }
+
--- app/models/User.java
+++ app/models/User.java
@@ -1,5 +1,49 @@
 package models;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Model;
+import play.db.jpa.Blob;
+
+@Entity
+public class User extends Model
+{
+	public boolean usaCitizen;
+	public String firstName;
+	public String lastName;
+
+	@OneToMany(mappedBy = "from", cascade = CascadeType.ALL)
+	List<Donation> donations = new ArrayList<Donation>();
+	public String email;
+	public String password;
+
+	public User(boolean usaCitizen, String firstName,
+			String lastName, String email, String password)
+	{
+		this.usaCitizen = usaCitizen;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public static User findByEmail(String email)
+	{
+		return find("email", email).first();
+	}
+
+	public boolean checkPassword(String password)
+	{
+		return this.password.equals(password);
+	}
 }
+
+

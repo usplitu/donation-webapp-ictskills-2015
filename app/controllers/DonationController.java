e9ab2152f078a72ad8755ef524ab8fe20b882644
Jean Crosse <jeancrosse37@gmail.com>
2015-06-16 12:04:04 +0100

    v2 Added real Donation page

--- app/controllers/DonationController.java
+++ app/controllers/DonationController.java
@@ -9,8 +9,11 @@ import models.*;
 public class DonationController extends Controller
 {
 	public static void index()
-	{
+	{   
+		
 		User user = Accounts.getCurrentUser();
+		
+				
 		if (user == null)
 		{
 			Logger.info("Donation class : Unable to getCurrentuser");
@@ -18,15 +21,15 @@ public class DonationController extends Controller
 		}
 		else
 		{
-			render();
-			//String prog = getPercentTargetAchieved();
-			//String progress = prog + "%";
-			//Logger.info("Donation ctrler : user is " + user.email);
-			//Logger.info("Donation ctrler : percent target achieved " + progress);
-			//render(user, progress);
+			
+			String prog = getPercentTargetAchieved();
+			String progress = prog + "%";
+			Logger.info("Donation ctrler : user is " + user.email);
+			Logger.info("Donation ctrler : percent target achieved " + progress);
+			render(user, progress);
 		}
 	}
-	
+
 	public static void donate(long amountDonated, String methodDonated)
 	{
 		Logger.info("amount donated " + amountDonated + " " + "method donated " + methodDonated);
@@ -45,34 +48,34 @@ public class DonationController extends Controller
 
 	private static void addDonation(User user, long amountDonated, String methodDonated)
 	{
-		//Donation bal = new Donation(user, amountDonated, methodDonated);
-		//bal.save();
+		Donation bal = new Donation(user, amountDonated, methodDonated);
+		bal.save();
 	}
-	
-	//private static long getDonationTarget()
-	//{
-	//	return 20000;
-	//}
-	
-	//public static String getPercentTargetAchieved()
-	//{
-		//List<Donation> allDonations = Donation.findAll();
-		//long total = 0;
-		//for (Donation donation : allDonations)
-		//{
-		//	total += donation.received;
-		//}
-		//long target = getDonationTarget();
-		//long percentachieved = (total * 100 / target);
-		//String progress = String.valueOf(percentachieved);
-		//Logger.info("Percent of target achieved (string) " + progress + 
-		//		"percentachieved (long)= " + percentachieved);
-		//		return progress;
-	//}
-	
+
+	private static long getDonationTarget()
+	{
+		return 20000;
+	}
+
+	public static String getPercentTargetAchieved()
+	{
+		List<Donation> allDonations = Donation.findAll();
+		long total = 0;
+		for (Donation donation : allDonations)
+		{
+			total += donation.received;
+		}
+		long target = getDonationTarget();
+		long percentachieved = (total * 100 / target);
+		String progress = String.valueOf(percentachieved);
+		Logger.info("Percent of target achieved (string) " + progress + 
+				"percentachieved (long)= " + percentachieved);
+		return progress;
+	}
+
 	public static void renderReport()
 	{
-		//List<Donation> donations = Donation.findAll();
-		//render(donations);
+		List<Donation> donations = Donation.findAll();
+		render(donations);
 	}
 }
